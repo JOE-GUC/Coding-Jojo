83f8129b2c381089ab8abab77ba3ea56a314adc3
JOSEPH-JOSE <joeboy65458@agmail.com>
2024-05-22 09:41:09 +0100

    done

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,12 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+const path = require('path');
+
+module.exports = {
+    webpack: (config, { isServer }) => {
+        config.resolve.alias['@'] = path.resolve(__dirname);
+        return config;
+    },
+};
